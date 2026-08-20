cd744780be757db6679eafe2f68b7d6228d67c5b
Chukwujekwu Michael <114012441+Chukwujekwu1@users.noreply.github.com>
2026-08-20 15:39:25 +0100

    added comments

--- create_schema.sql
+++ create_schema.sql
@@ -1,3 +1,6 @@
+-- This script creates the bronze, silver, and gold schemas
+-- if they don't already exist
+
 CREATE SCHEMA IF NOT EXISTS bronze;
 CREATE SCHEMA IF NOT EXISTS silver;
 CREATE SCHEMA  IF NOT EXISTS gold;
